5e09d33b5d455fcbe7de7709ec9486e8f4afda08
NoFunny <chunikhin_1999@mail.ru>
2020-09-06 15:44:55 +0700

    Fix input data for methods

--- ZI/lab1.py
+++ ZI/lab1.py
@@ -9,18 +9,33 @@ def isPrime(n):
     else:
         return True
 
-def fast_exponentiation(a, x, m):
+def check_gen(p):
+    if p <= 2:
+        return False
+    tmp = p - 1
+    q = tmp//2
+    if tmp%2 != 0 or not(isPrime(q)):
+        return False    
+    g = p-2
+    while fast_exponentiation(g,q,p) == 1:
+        g -= 1
+    if g <= 1:
+        return False
+    return g
+
+
+def fast_exponentiation(base, x, mod):
     y = 1
-    # a * d mod m = 1
+    # base * d mod m = 1
     if x < 0:
         # search d
-        a = ewklid(a,m)[0]
+        base = ewklid(base,mod)[0]
         x = -x
-    s = a
+    s = base
     while x:
         if x&1:
-            y = y*s % m
-        s *= s % m
+            y = y*s % mod
+        s *= s % mod
         x >>= 1
     return y
 
@@ -39,35 +54,40 @@ def ewklid(a,b):
         y, y1 = y1, y - y1*q
     return x, y, a
 
-def diffie_hellman(base, privat_key, public_key, mod):
-    if isPrime(base) and isPrime(mod):
-        return fast_exponentiation(base, privat_key*public_key, mod)
+def diffie_hellman(private_key, public_key, mod):
+    if public_key < mod:
+        return fast_exponentiation(public_key,private_key, mod)
     else:
-        print('base or mod is not prime')
+        print('Public key greatest than mod!')
 
-# def gelfond_shenx():
+def gelfond_shanks():
 
 
 def main():
-    # print('for ewklid')
-    # print('a =')
-    # a = int(input())
-    # print('b =')
-    # b = int(input())
-    # print('| x | y | ewklid |')
-    # print(ewklid(a, b))
-    # print('for POW')
-    print('base =')
-    base = int(input())
-    print('privat_key =')
-    privat_key = int(input())
-    print('public_key =')
-    public_key = int(input())
+    print('enter mod(prime number):')
+    tmp = int(input())
+    mod = 0
+    if isPrime(tmp):
+        mod = tmp * 2 + 1
+    print('mod = ',mod)
+    tmp = check_gen(mod)
+    if tmp:
+        base = tmp
+    print('base = ',base)
+    print('my_privat_key =')
+    my_private_key = int(input())
+    my_public_key = fast_exponentiation(base, my_private_key, mod)
+    print('my_public_key = ',my_public_key)
+    print('eva_privat_key =')
+    eva_public_key = int((input()))
+    print('pow = ', fast_exponentiation(base, my_private_key, mod))
+    print('ewklid = ', ewklid(100500900,200400500))
+    print('DiffieHellman = ', diffie_hellman(eva_public_key,my_private_key, mod))
     # print('x =')
     # x = int(input())
-    print('m =')
-    mod = int(input())
-    print(diffie_hellman(base,privat_key,public_key,mod))
+    # print('m =')
+    # mod = int(input())
+    # print(diffie_hellman(my_privat_key,public_key,mod))
     # print('pow')
     # print(fast_exponentiation(a,x,p))
 
