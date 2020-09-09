59a942535abbd53bf8c304e800f5e0c9640170dc
ZinkPavel <tsynk.p@gmail.com>
2020-09-09 16:15:40 +0700

    Refactoring

--- ZI/lab1.py
+++ ZI/lab1.py
@@ -1,101 +1,102 @@
 #!/usr/bin/python3
+import sys
 import math
+import random
 
-
-#Villson Theorem
 def isPrime(n):
     if (n <= 2): return False
     for num in range(2, math.floor(math.sqrt(n))):
         if (n % num == 0): return False
     return True
 
-def check_genP(p):
-    if p <= 2:
-        return False
-    tmp = p - 1
-    q = tmp//2
-    if tmp%2 != 0 or not(isPrime(q)):
-        return False    
-    g = p-2
-    while fast_exponentiation(g,q,p) == 1:
-        g -= 1
-    if g <= 1:
-        return False
-    return g
+def fastModuloExponentiation(a, x, p):
+    result = 1
+    t = math.floor(math.log2(x))
+    tmp = a
+    for num in range(t + 1):
+        if num > 0: tmp *= tmp % p
 
+        if (x % 2) == 1:
+            result *= tmp
+        x //= 2
 
-def fast_exponentiation(base, x, mod):
-    y = 1
-    # base * d mod m = 1
-    if x < 0:
-        # search d
-        base = ewklid(base,mod)[0]
-        x = -x
-    s = base
-    while x:
-        if x&1:
-            y = y*s % mod
-        s *= s % mod
-        x >>= 1
-    return y
+    return result % p
+        
 
-def ewklid(a,b):
-    if a < 0 or b < 0:
-        return 'Incorrect data'
-    if a == 0:
-        x = 0
-        y = 1
-        return b, 0, 1
-    x, x1, y, y1 = 1, 0, 0, 1
-    while b:
-        q = a // b
-        a, b = b, a % b
-        x, x1 = x1, x - x1*q
-        y, y1 = y1, y - y1*q
-    return x, y, a
-
-def diffie_hellman(private_key, public_key, mod):
-    if public_key < mod:
-        return fast_exponentiation(public_key,private_key, mod)
-    else:
-        print('Public key greatest than mod!')
-
-# def gelfond_shanks():
+def genEuclideanAlgo(a, b):
+    if b > a: a, b = b, a
     
+    U, V = [a, 1, 0], [b, 0, 1]
+
+    while (V[0] != 0):
+        tmp = U[0] // V[0]
+        tmpList = [U[0] % V[0], U[1] - tmp * V[1], U[2] - tmp * V[2]]
+        U, V = V, tmpList
+    return U
+
+def diffieHellmanProtocol(p, g):
+    xA, xB = random.randint(1, 10), random.randint(1, 10)
+    yA, yB = fastModuloExponentiation(g, xA, p), fastModuloExponentiation(g, xB, p)
+    zA, zB = fastModuloExponentiation(yA, xA, p), fastModuloExponentiation(yB, xB, p)
+    return zA == zB
+
+def firstEntry(list, x): 
+    index = 0
+    
+    for elem in list:
+        if elem == x: return index
+        index += 1
+
+    return -1
+
+def babyGiantStep(a, p, y):
+    # random generate m & k
+    m, k = random.randint(1, 10) * p / 2, random.randint(1, 10) * p / 2    
+    # m, k = 6, 4
+    # --------------
+
+    result = [0, 0]
+    iList, jList = [fastModuloExponentiation(a, m, p)], [y]
+
+    for j in range(1, m):
+        jList.append((a * jList[j-1]) % p) # in this case faster than call.fastModuloExponentiation()
+
+    for i in range(1, k - 1):
+        result[1] = firstEntry(jList, iList[i - 1])
+        if result[1] != -1: 
+            result[0] = i
+            break
+        iList.append((iList[i-1] * iList[i-1]) % p)
+    
+    return result[0] * m - result[1]
 
 def main():
-    # EWKLID
-    print('ewklid = ', ewklid(100500900,200400500))
-    
-    #POW
-    print('enter base:')
-    base = int(input())
-    print('enter x:')
-    x = int(input())
-    print('enter mod:')
-    mod = int(input())
-    print('pow = ', fast_exponentiation(base, x, mod))
-    
-    #DIFFIE HELLMAN
-    print('enter mod(prime number):')
-    tmp = int(input())
-    mod = 0
-    if isPrime(tmp):
-        mod = tmp * 2 + 1
-    print('mod for diffie hellman = ',mod)
-    tmp = check_genP(mod)
-    if tmp:
-        base = tmp
-    print('base = ',base)
-    print('my_privat_key =')
-    my_private_key = int(input())
-    my_public_key = fast_exponentiation(base, my_private_key, mod)
-    print('my_public_key = ',my_public_key)
-    print('eva_privat_key =')
-    eva_public_key = int((input()))
-    print('DiffieHellman = ', diffie_hellman(eva_public_key,my_private_key, mod))
+    # Section №1 --- # Fast modulo expotential
+    p, a, x = int(input())
 
-    #GELFOND SHANKS
+    if not isPrime(p): sys.exit('ERROR. You entered a composite number. Try again.')
+    
+    print(fastModuloExponentiation(a, x, p))
+
+    # Section №2 --- # Generalized Euclidean algorithm.
+    a, b = int(input()), int(input())
+
+    gcd, x, y = genEuclideanAlgo(a, b)
+
+    print('GCD = ', gcd, 'X = ', x, 'Y = ', y)
+
+    # Section №3 --- # Diffi-Hallman protocol. Generating common key's.
+    p, g = int(input()), int(input())
+
+    print(diffieHellmanProtocol(p, g))
+
+    # Section №4 --- # Baby step, Giant step algorithm.
+    a, p, y = int(input()), int(input()), int(input())
+
+    result = babyGiantStep(a, p, y)
+
+    if fastModuloExponentiation(a, result, p) == y: print(a, '^', result, ' mod ', p, ' = ', y)
+    else: print('Verification failed')
 
 if __name__ == "__main__":
     main()
